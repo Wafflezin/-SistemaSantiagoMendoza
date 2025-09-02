c5434af6c46b8a026fccb9558e7c26e92e0690bb
Wafflezin <santiarieldm@gmail.com>
2025-09-02 14:18:01 -0400

    Coloquei tudo o que foi dado em aula até o instance of no meu projeto.

---
+++ src/tools/Sad_Util.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tools;
+
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.text.JTextComponent;
+
+/**
+ *
+ * @author u70874542189
+ */
+public class Sad_Util {
+
+    public static void sad_habilitar(boolean valor, JComponent... componentes) {
+        for (int i = 0; i < componentes.length; i++) {
+            componentes[i].setEnabled(valor);
+
+        }
+    }
+
+    public static void sad_limpar(JComponent... componentes) {
+        for (int i = 0; i < componentes.length; i++) {
+            JComponent componente = componentes[i];
+
+            if (componente instanceof JTextComponent) {
+                ((JTextComponent) componente).setText("");
+
+            } else if (componente instanceof JComboBox) {
+                ((JComboBox<?>) componente).setSelectedIndex(-1);
+
+            } else if (componente instanceof JCheckBox) {
+                ((JCheckBox) componente).setSelected(false);
+            }
+
+        }
+    }
+};
